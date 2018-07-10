b9be2d14a3075775b488f4eb7429115204d41aca
Mário Gil <mg_mesquita@hotmail.com>
2018-07-10 19:54:53 +0100

    Checking if arguments are provided and organizing code in functions

--- mac_changer.py
+++ mac_changer.py
@@ -3,18 +3,22 @@
 import subprocess
 import optparse
 
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface. Use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify a new MAC address. Use --help for more info.")
+    return options
+
 def change_mac(interface, new_mac):
     print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
     subprocess.call(["ifconfig", interface, "up"])
 
-parser = optparse.OptionParser()
-
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="")
-
-(options, arguments) = parser.parse_args()
-
+options = get_arguments()
 change_mac(options.interface, options.new_mac)
