41bc604e00e0544f3fbbe705bd3d9e22d642f9a0
Mário Gil <mg_mesquita@hotmail.com>
2018-07-10 17:39:03 +0100

    Used optparse and cleaned up code

--- mac_changer.py
+++ mac_changer.py
@@ -3,17 +3,18 @@
 import subprocess
 import optparse
 
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
 parser = optparse.OptionParser()
 
 parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-parser.parse_args()
+parser.add_option("-m", "--mac", dest="new_mac", help="")
 
-interface = raw_input("interface > ")
-new_mac = raw_input("new MAC > ")
+(options, arguments) = parser.parse_args()
 
-print("[+] Changing MAC address for " + interface + " to " + new_mac);
-
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
