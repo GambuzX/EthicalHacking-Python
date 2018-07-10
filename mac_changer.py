42a4aaed1891ff103ce0139ad139ee9d4d6afc7f
Mário Gil <mg_mesquita@hotmail.com>
2018-07-10 15:52:35 +0100

    Simple mac changer

--- mac_changer.py
+++ mac_changer.py
@@ -2,4 +2,6 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+subprocess.call("ifconfig wlan0 down", shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
