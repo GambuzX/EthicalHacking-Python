a0e3feca7d74b6175a0b87e119fed99a39805a38
Mário Gil <mg_mesquita@hotmail.com>
2018-07-10 16:46:19 +0100

    Update

--- mac_changer.py
+++ mac_changer.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-subprocess.call("ifconfig wlan0 down", shell=True)
-subprocess.call("ifconfig wlan0 hw ether 00:11:22:33:44:55", shell=True)
-subprocess.call("ifconfig wlan0 up", shell=True)
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+parser.parse_args()
+
+interface = raw_input("interface > ")
+new_mac = raw_input("new MAC > ")
+
+print("[+] Changing MAC address for " + interface + " to " + new_mac);
+
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
