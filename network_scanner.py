ecb6632c25d71d5297aa968e07539d317b951820
Mário Gil <mg_mesquita@hotmail.com>
2018-07-15 17:57:56 +0100

    Implemented optparse

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--ip", dest="ip_address", help="IP range to scan for devices.")
+    (options, arguments) = parser.parse_args()
+    if not options.ip_address:
+        parser.error("[-] Please specify an IP range. Use --help for more info.")
+    return options.ip_address
 
 
 def scan(ip):
@@ -13,4 +23,6 @@ def scan(ip):
     for element in answered_list:
         print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
-scan("192.168.1.1/24")
+
+ip = get_arguments()
+scan(ip)
