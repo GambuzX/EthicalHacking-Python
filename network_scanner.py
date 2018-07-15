28eac181d35a2fd32583268964c5d65dfe1b6eb2
Mário Gil <mg_mesquita@hotmail.com>
2018-07-15 16:11:33 +0100

    Parsing response for wanted parts.

--- network_scanner.py
+++ network_scanner.py
@@ -5,8 +5,12 @@ import scapy.all as scapy
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = arp_request/broadcast
-    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
-    print(answered.summary())
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
 
-scan("10.0.2.1/24")
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("---------------------------------------------------------------")
+
+scan("192.168.1.1/24")
