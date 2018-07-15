ce2015ecb6246dbf84791c7567b20b1f6846fe42
Mário Gil <mg_mesquita@hotmail.com>
2018-07-15 12:37:42 +0100

    Creating my custom arp packet

--- network_scanner.py
+++ network_scanner.py
@@ -3,6 +3,8 @@
 import scapy.all as scapy
 
 def scan(ip):
-    scapy.arping(ip);
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = arp_request/broadcast
 
-scan("10.0.2.1/24");
+scan("10.0.2.1/24")
