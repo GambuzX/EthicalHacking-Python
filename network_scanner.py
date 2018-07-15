c5961f4cfe3f3ef1aafcb6f516d19581dbef61d0
Mário Gil <mg_mesquita@hotmail.com>
2018-07-15 12:59:22 +0100

    Sending and Receiving Packets

--- network_scanner.py
+++ network_scanner.py
@@ -6,5 +6,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = arp_request/broadcast
+    answered, unanswered = scapy.srp(arp_request_broadcast, timeout=1)
+    print(answered.summary())
 
 scan("10.0.2.1/24")
