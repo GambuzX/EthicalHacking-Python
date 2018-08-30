a18f3c393276fcf979acf82c44a6fe59873c840d
Mário Gil <mg_mesquita@hotmail.com>
2018-08-30 09:02:15 +0100

    Crawler now also scans for directories and files

--- crawler.py
+++ crawler.py
@@ -11,6 +11,9 @@ def request(url):
 
 
 target_url = "google.com"
+
+print("Searching for SUBDOMAINS")
+print("======================")
 with open("/root/Downloads/subdomains.list", "r") as wordlist_file:
     for line in wordlist_file:
         word = line.strip()
@@ -18,3 +21,14 @@ with open("/root/Downloads/subdomains.list", "r") as wordlist_file:
         response = request(test_url)
         if response:
             print("[+] Discovered subdomain --> " + test_url)
+
+
+print("\nSearching for DIRECTORIES")
+print("======================")
+with open("/root/Downloads/files-and-dirs-wordlist.txt", "r") as wordlist_file:
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = target_url + "/" + word
+        response = request(test_url)
+        if response:
+            print("[+] Discovered URL --> " + test_url)
