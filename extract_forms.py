7221374bbc0f3d35a1e40e93721b4fab550a0aa3
Mário Gil <mg_mesquita@hotmail.com>
2018-08-31 10:17:33 +0100

    Posting data

--- extract_forms.py
+++ extract_forms.py
@@ -2,6 +2,7 @@
 
 import requests
 from BeautifulSoup import BeautifulSoup
+import urlparse
 
 
 def request(url):
@@ -19,9 +20,18 @@ forms_list = parsed_html.findAll("form")
 
 for form in forms_list:
     action = form.get("action")
+    post_url = urlparse.urljoin(target_url, action)
     method = form.get("method")
 
     inputs_list = form.findAll("input")
+    post_data = {}
     for input in inputs_list:
         input_name = input.get("name")
-        print(input_name)
+        input_type = input.get("type")
+        input_value = input.get("value")
+        if input_type == "text":
+            input_value = "test"
+
+        post_data[input_name] = input_value
+    result = requests.post(post_url, data=post_data)
+    print(result.content)
